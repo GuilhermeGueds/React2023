e888b1635322e34c1d9034ff313ef7be66f53877
GuilhermeGueds <guilhermeguedes.cc@gmail.com>
2023-05-31 21:03:24 -0300

    criado estrutura do projeto

---
+++ 01-fundamentos-react-js/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import headerStyled from "./Header.module.css";
+export function Header() {
+  return (
+    <header className={headerStyled.header}>
+      <strong>Header</strong>
+    </header>
+  );
+}
